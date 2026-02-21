35c8a591816eed0e103255aea7a10a9ccaf3c26c
Richard <richardleichter80@gmail.com>
2026-02-21 12:06:21 -0300

    Implementação do codigo banco

--- Gemfile
+++ Gemfile
@@ -3,3 +3,12 @@ source 'https://rubygems.org'
 gem 'colorize'
 gem 'httparty'
 gem 'json'
+
+group :development do
+  gem "ruby-lsp", require: false
+end
+
+group :development, :test do
+  gem "rubocop", require: false
+  gem "rubocop-performance", require: false # Opcional, mas bom ter
+end
--- lib/banco.rb
+++ lib/banco.rb
@@ -51,5 +51,59 @@ class Banco
     cpf_limpo = cpf.gsub(/\D/, '')
     @clientes.find { |c| c.cpf == cpf_limpo}
   end
-    
 
+  def buscar_cliente_por_numero_conta(numero_conta)
+    @clientes.find { |c| c.conta.numero_conta == numero_conta}
+  end
+
+  def login(numero_conta:, senha:)
+    cliente = buscar_cliente_por_conta(numero_conta)
+
+    unless cliente
+      return { sucesso: false, erro: "Conta não encontrada" }
+    end
+
+    unless cliente.autenticador(senha)
+      return { sucesso: false, erro: "Senha incorreta" }
+    end
+
+    {sucesso: true, cliente: cliente}
+  end
+
+  def salvar_dados
+    dados = {
+      nome: @nome,
+      cliente: @cliente.map(&:to_hash)
+    }
+
+    File.write(ARQUIVO_DADOS, JSON.pretty_generate(dados))
+  end
+
+  def carregar_dados
+    return unless File.exist?(ARQUIVO_DADOS)
+
+    dados = JSON.parse(File.read(ARQUIVO_DADOS))
+
+    @CLIENTES = dados['clientes'].map do |hash|
+      Cliente.from_hash(hash)
+    end
+  rescue => e
+    puts "Erro ao carregar dados: #{e.message}".colorize(:red)
+  end
+
+  def estatisticas
+    total_clientes= @clientes.length
+    saldo_total =@clientes.sum { |c| c.conta.transacoes.legth }
+    total_transacoes = @clientes.sum { |c| c.conta.transacoes.length }
+
+    stats = "\n" + "=" * 60 + "\n"
+    stats += "📈 ESTATÍSTICAS DO BANCO #{@nome}".center(60).colorize(:cyan).bold + "\n"
+    stats += "=" * 60 + "\n"
+    stats += "Total de clientes: #{total_clientes}\n"
+    stats += "Saldo total: R$ #{sprintf('%.2f', saldo_total)}".colorize(:green) + "\n"
+    stats += "Total de transações: #{total_transacoes}\n"
+    stats += "=" * 60
+    
+    stats
+  end
+end
